89ba68251a198d7f44d438b2c3d6002cd996d367
Alexander <alex3747@abv.bg>
2024-09-12 13:34:58 +0300

    adding team details view

--- frontend/src/components/TeamDetails.jsx
+++ frontend/src/components/TeamDetails.jsx
@@ -1,9 +1,52 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function TeamDetails() {
     const {teamId} = useParams();
 
+    const [team, setTeam] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch(`http://localhost:8080/teams/${teamId}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setTeam(data);
+                setLoading(false)
+            })
+            .catch((error) => {
+                console.error('Error fetching teams:', error);
+            });
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+
+    console.log(team)
+
+
     return (
-        <h1>Team with {teamId}</h1>
+        <div className="teams-container">
+            <h1>{team[0].teamName}</h1>
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>Team Number</th>
+                        <th>Player Name</th>
+                        <th>Position</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {team.map((team) => (
+                            <tr key={team.id}>
+                                <td>{team.playerNumber}</td>
+                                <td>{team.playerName}</td>
+                                <td>{team.playerPosition}</td>
+                            </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     )
 }
